dc7f878a36f74dab6461f4e3ba7b9fb63134ac3a
Gabriel-Prd <gabrielpradomartinsgbi@gmail.com>
2024-01-20 12:24:34 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import tecnoImg from './Assets/FullLogoWhite.svg'
+import './styles.css'
 
 function App() {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='imgTecno'>
+        <img src={tecnoImg} alt='TecnoJr'></img>
+      </div>
+
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+
+            <span className="login-form-title">Dashboard</span>
+
+            <div className='wrap-input'>
+              <input 
+                className= {username !== "" ? "has-val input" : "input"}
+                type='text'
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+              <span className='focus-input' data-placeholder="Username"></span>
+            </div>
+
+            <div className='wrap-input'>
+              <input 
+                className={password !== "" ? "has-val input" : "input"}
+                type='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                />
+              <span className='focus-input' data-placeholder="Password"></span>
+            </div>
+
+            <div className='container-login-form-btn'>
+              <button className='login-form-btn'>Login</button>
+            </div>
+            
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
